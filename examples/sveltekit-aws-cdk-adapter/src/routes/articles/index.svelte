bfde95e6baa3ef529f890fcd032a9c6c63dcee74
Mike Bild <mike.bild.ext@siemens.com>
2022-02-21 20:32:57 +0100

    aws-cdk adapter

---
+++ examples/sveltekit-aws-cdk-adapter/src/routes/articles/index.svelte
@@ -0,0 +1,6 @@
+<script context="module">
+  export const prerender = true;
+  export const hydrate = false;
+</script>
+
+<h1>Articles</h1>
---
+++ examples/sveltekit-aws-cdk-adapter/src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
