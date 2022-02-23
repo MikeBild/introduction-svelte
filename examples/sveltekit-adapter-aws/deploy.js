63122d88bbafb297b5e89518e7120d8b2c249547
Mike Bild <mike.bild.ext@siemens.com>
2022-02-23 07:35:17 +0100

    more

--- examples/sveltekit-adapter-aws/deploy.js
+++ examples/sveltekit-adapter-aws/deploy.js
@@ -5,8 +5,6 @@ import { AWSAdapterStack } from 'sveltekit-adapter-aws';
 import { IntrastructureStack } from './infrastructure.js';
 
 const app = new App();
-app.region = 'us-east-1';
-app.account = process.env.CDK_DEFAULT_ACCOUNT;
 
 const { serverHandler } = new AWSAdapterStack(app, 'AWSAdapterStack', {
   FQDN: 'demo.mikebild.com',
