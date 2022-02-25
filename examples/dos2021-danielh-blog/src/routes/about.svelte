38707d7b86a948841f27e0d50665af3cdebaf750
Daniel <daniel.herzog@softwareone.com>
2022-02-25 10:32:27 +0100

    changed loading

--- examples/dos2021-danielh-blog/src/routes/about.svelte
+++ examples/dos2021-danielh-blog/src/routes/about.svelte
@@ -1,1 +1,7 @@
+<script context="module">
+	export const prerender = true;
+	export const hydrate = false;
+	export const router = false;
+</script>
+
 <h1>About</h1>
--- examples/dos2021-danielh-blog/src/routes/index.svelte
+++ examples/dos2021-danielh-blog/src/routes/index.svelte
@@ -1,7 +1,2 @@
-<script context="module">
-    export const prerender = true;
-    export const hydrate = false;
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <a href="/about">About</a>
